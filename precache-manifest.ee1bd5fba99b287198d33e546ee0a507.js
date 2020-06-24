03989a9541a7e981cfab5a506ce8727cc7fbca2c
Naoki Kojima <dark.light9640@gmail.com>
2020-06-24 22:07:34 +0900

    Updates

---
+++ precache-manifest.ee1bd5fba99b287198d33e546ee0a507.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c1d3c86ef39b1d4c4aef0e2bde33d8",
+    "url": "//Naok000.github.io/pokeList/index.html"
+  },
+  {
+    "revision": "896023d973449db47fb8",
+    "url": "//Naok000.github.io/pokeList/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b66dfb7cc61b19730952",
+    "url": "//Naok000.github.io/pokeList/static/css/main.2ea2eaa2.chunk.css"
+  },
+  {
+    "revision": "896023d973449db47fb8",
+    "url": "//Naok000.github.io/pokeList/static/js/2.c6d33fe5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//Naok000.github.io/pokeList/static/js/2.c6d33fe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b66dfb7cc61b19730952",
+    "url": "//Naok000.github.io/pokeList/static/js/main.0c2f5c28.chunk.js"
+  },
+  {
+    "revision": "a949c8c0427d51cbbcfb",
+    "url": "//Naok000.github.io/pokeList/static/js/runtime-main.87b0fc31.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "//Naok000.github.io/pokeList/static/media/SEGA.12a3db8b.woff"
+  }
+]);
